ff4ddbf33049b810b64d9abdc8099a19d63ab032
shintabocill <shintarizkiayuutami@gami.com>
2025-05-28 22:42:00 +0700

    json ke .objek

--- json/script.js
+++ json/script.js
@@ -8,6 +8,7 @@
 
 console.log(JSON.stringify(mahasiswa));
 
+//json ke. objek
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
