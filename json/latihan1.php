74c8fc67ee8d1d156370ce82bbcdbd46574af4f6
shintabocill <shintarizkiayuutami@gami.com>
2025-05-28 18:27:49 +0700

    tess commit

---
+++ json/latihan1.php
@@ -0,0 +1,26 @@
+<?php
+
+// $data_mahasiswa = [
+//     [
+//         "nama" => "Shintarizki",
+//         "nim" => "2217020034",
+//         "email" => "shintarizkiayuutami@gmail.com"
+//     ],
+//     [   
+//         "nama" => "ayuutami",
+//         "nim" => "2217020034",
+//         "email" => "shintarizki@gmail.com"
+// 
+// 
+// ];
+
+$dbh = new PDO('mysql:host=localhost;dbname=db_mahasiswa', 'root', '');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+?>
